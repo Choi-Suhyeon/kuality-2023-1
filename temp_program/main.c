f19a9647afcd6c5e26fe916d645de3b4bda46bf8
Choi-Suhyeon <84931527+Choi-Suhyeon@users.noreply.github.com>
2023-03-18 10:28:22 +0900

    modify the code

--- temp_program/main.c
+++ temp_program/main.c
@@ -37,9 +37,9 @@ bool playUpDownGame(const Level * const kLvl) {
 }
 
 int main(void) {
-    const Level easy = { .kMaxNum = 100, .kMaxCnt = 15 },
-                itrm = { .kMaxNum = 150, .kMaxCnt = 13 },
-                hard = { .kMaxNum = 200, .kMaxCnt = 11 };
+    const Level kEasy = { .kMaxNum = 100, .kMaxCnt = 15 },
+                kItrm = { .kMaxNum = 150, .kMaxCnt = 13 },
+                kHard = { .kMaxNum = 200, .kMaxCnt = 11 };
 
     char ifToQuit = '\0',
          level    = '\0';
@@ -51,9 +51,9 @@ int main(void) {
         scanf("%c", &level);
         
         puts((NULL ||
-            level == '1' || level == 'E' ? playUpDownGame(&easy) :
-            level == '2' || level == 'I' ? playUpDownGame(&itrm) :
-            level == '3' || level == 'H' ? playUpDownGame(&hard) :
+            level == '1' || level == 'E' ? playUpDownGame(&kEasy) :
+            level == '2' || level == 'I' ? playUpDownGame(&kItrm) :
+            level == '3' || level == 'H' ? playUpDownGame(&kHard) :
             false) 
                 ? "Congratulations. You Won!"
                 : "Sorry, You Lose TT");
