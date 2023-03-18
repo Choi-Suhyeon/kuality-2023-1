22acbbfbd51c0639192be3bec5529d93d28dc089
Choi-Suhyeon <84931527+Choi-Suhyeon@users.noreply.github.com>
2023-03-18 10:17:12 +0900

    2nd Essential Task

---
+++ README.md
@@ -0,0 +1,1 @@
+TEMP PROGRAM : 2주차 과제(scanf를 사용한 프로그램)
---
+++ temp_program/main.c
@@ -0,0 +1,78 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    const unsigned kMaxNum,
+                   kMaxCnt;
+} Level;
+
+const char * const kQuitMenu  = "What do you want next?\n 1. [S]tart New Game\n 2. [Q]uit This Game\n> ",
+           * const kLevelMenu = "Select a Level.\n 1. [E]asy\n 2. [I]intermediate\n 3. [H]ard\n> ";
+
+static inline void flush(void) {
+    while (getchar() != '\n');
+}
+
+bool playUpDownGame(const Level * const kLvl) {
+    srand((unsigned) time(NULL));
+    
+    const unsigned kRandNum = rand() % (kLvl->kMaxNum + 1);
+
+    unsigned count = kLvl->kMaxCnt,
+             input = 0;
+
+    while (count--) {
+        printf("\nNUMBER > ");
+        scanf("%u", &input);
+
+        if      (input > kLvl->kMaxNum) puts("[E] OUT OF RANGE VAL");
+        else if (input > kRandNum)      puts("[-] DOWN");
+        else if (input < kRandNum)      puts("[+] UP");
+        else                            return true;
+    }
+
+    return false;
+}
+
+int main(void) {
+    const Level easy = { .kMaxNum = 100, .kMaxCnt = 15 },
+                itrm = { .kMaxNum = 150, .kMaxCnt = 13 },
+                hard = { .kMaxNum = 200, .kMaxCnt = 11 };
+
+    char ifToQuit = '\0',
+         level    = '\0';
+
+    puts("\t[Up Down Game]\n");
+
+    while (true) {
+        printf("%s", kLevelMenu);
+        scanf("%c", &level);
+        
+        puts((NULL ||
+            level == '1' || level == 'E' ? playUpDownGame(&easy) :
+            level == '2' || level == 'I' ? playUpDownGame(&itrm) :
+            level == '3' || level == 'H' ? playUpDownGame(&hard) :
+            false) 
+                ? "Congratulations. You Won!"
+                : "Sorry, You Lose TT");
+
+        while (true) {
+            flush();
+            printf("\n%s", kQuitMenu);
+            scanf("%c", &ifToQuit);
+            
+            if (ifToQuit == '1' || ifToQuit == 'S') break;
+
+            if (ifToQuit == '2' || ifToQuit == 'Q') goto PROGRAM_END; 
+        }
+
+        flush();
+    }
+
+    PROGRAM_END:
+    puts("Thank you for playing~");
+
+    return 0;
+}
